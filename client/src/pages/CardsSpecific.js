a5610c92ec3745fac8fcb25843c1497e2692d98d
Bjorn-Ivar <bjorn-ivar.skuggen@hotmail.com>
2020-03-19 22:44:31 +0100

    issues with this.props.match.params.id on CardSpecific.js

--- client/src/pages/CardsSpecific.js
+++ client/src/pages/CardsSpecific.js
@@ -1,60 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import axios from 'axios';
-import { RM_API } from './../components/constants/constants';
+import { RM_API,
+         HEROKU_BYPASS_CORS
+         } from './../components/constants/constants';
 import { Link } from 'react-router-dom';
 import CardClick from '../components/card-specific';
 
-export default class CardSpecific extends Component {
-    state = {
-        characterResult: undefined,
-    }
+export default function CardSpecific() {
+    const [characterId, setCharacterId] = useState(this.props.match.params.id)
+    const [characterResult, setCharacterResult] = useState(undefined);
 
-    componentDidMount() {
-        console.log('match', this.props.match)
-
-        axios.get(RM_API + this.props.match.params.id)
-            .then(result => {
-                console.log(result)
-                this.setState({
-                    characterResult: result.data,
-                })
+    useEffect(() => {
+        axios.get(HEROKU_BYPASS_CORS + RM_API + characterId)
+            .then((result) => {
+                setCharacterResult(result.data.results);
             })
-            .catch((err) => {
-                console.log(err)
-            })
-    }
+    }, [])
 
-    render() {
-        const { characterResult } = this.state;
-        return (
-            <div>
-                <div className='row'>
-                    <div className='col-sm-12'>
-                        <h1 className='heading'>Card Specific</h1>
-                    </div>
-                    <div className='col-sm-12'>
-                        {
-                            (characterResult !== undefined) ?
-                                <CardClick
-                                    name={characterResult.name}
-                                    image={characterResult.image}
-                                    gender={characterResult.gender}
-                                    species={characterResult.species}
-                                    status={characterResult.status}
-                                    origin={characterResult.origin.name}
-                                    created={characterResult.created}
-                                /> :
-                                <div className='d-flex justify-content-center col-md-6'>
-                                    <img className='w-100' src='https://flevix.com/wp-content/uploads/2019/07/Bubble-Preloader-1.gif' alt='loading' />
-                                </div>
-                        }
-                    </div>
+
+
+    return (
+        <div>
+            <div className='row'>
+                <div className='col-sm-12'>
+                    <h1 className='heading'>Card Specific</h1>
                 </div>
-                <div className='d-flex justify-content-center mt-5'>
-                    <button className='btn btn-primary'><Link to='/'>{'Back to Dashboard'}</Link></button>
+                <div className='col-sm-12'>
+                    {
+                        (characterResult !== undefined) ?
+                            <CardClick
+                                name={characterResult.name}
+                                image={characterResult.image}
+                                gender={characterResult.gender}
+                                species={characterResult.species}
+                                status={characterResult.status}
+                                origin={characterResult.origin.name}
+                                created={characterResult.created}
+                            /> :
+                            <div className='d-flex justify-content-center col-md-6'>
+                                <img className='w-100' src='https://flevix.com/wp-content/uploads/2019/07/Bubble-Preloader-1.gif' alt='loading' />
+                            </div>
+                    }
                 </div>
             </div>
-        )
-    }
+            <div className='d-flex justify-content-center mt-5'>
+                <button className='btn btn-primary'><Link to='/'>{'Back to Dashboard'}</Link></button>
+            </div>
+        </div>
+    )
 }
