7cec4c413767b206f589d1094f81a22e1b87ff4e
Bjørn-Ivar Skuggen <bjorn-ivar.skuggen@hotmail.com>
2020-03-20 14:55:29 +0100

    removed class component from App.js... Deleted login functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,20 @@
-import React, { Component } from 'react';
-
+import { useRouteMatch} from "react-router";
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/styles.scss';
 import Navbar from './components/navbar';
 import Footer from './components/footer';
 
-import Login from './pages/Login';
 
-import {Link} from 'react-router-dom';
+export default function App(props) {
 
-
-export default class App extends Component {
-  state = {
-    isLoggedIn: true,
-  }
-
-  updateLogin = () => {
-    this.setState({
-      isLoggedIn: true,
-    })
-  }
-
-  logoutUser = () => {
-    this.setState({
-      isLoggedIn: false,
-    })
-    sessionStorage.clear();
-  }
-
-  render() {
-    return (sessionStorage.getItem('user') !== null && this.state.isLoggedIn) ? 
-    ( 
+    return (
       <div className='App'>
         <Navbar />   
-        <button className='btn btn-primary fixed-top btn-logout' onClick={this.logoutUser}>
-        <Link to={'/'}>Log out</Link> 
-        </button>
-          {this.props.children}
+          {props.children}
           <Footer />
       </div>
-    ) : (
-      <Login updateLoginStatus={this.updateLogin} />
     )
+
   }
-}
+
